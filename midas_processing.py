b29fff5bd0108ee71fccd4e9d19d337fd020eb15
Travis Smalling <main@smalling.dev>
2023-07-07 09:57:34 -0500

    added midas example code

---
+++ midas_processing.py
@@ -0,0 +1,52 @@
+import torch
+import urllib.request
+import cv2
+import time
+import matplotlib.pyplot as plt
+
+url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
+#urllib.request.urlretrieve(url, filename)
+
+#model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+#model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+
+midas = torch.hub.load("intel-isl/MiDaS", model_type)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+midas.to(device)
+midas.eval()
+
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+
+if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+    transform = midas_transforms.dpt_transform
+else:
+    transform = midas_transforms.small_transform
+
+start = time.time()
+
+img = cv2.imread(filename)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+input_batch = transform(img).to(device)
+
+with torch.no_grad():
+    prediction = midas(input_batch)
+
+    prediction = torch.nn.functional.interpolate(
+        prediction.unsqueeze(1),
+        size=img.shape[:2],
+        mode="bicubic",
+        align_corners=False,
+    ).squeeze()
+
+output = prediction.cpu().numpy()
+print("Time elapsed: ", time.time() - start)
+
+while True:
+    cv2.imshow("Camera", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
+#plt.imshow(output)
