2926f4d56d4290bc59c3a871df0d662073de6eb3
Travis Smalling <main@smalling.dev>
2023-07-11 13:23:18 -0500

    comment again

--- furniture_detection.py
+++ furniture_detection.py
@@ -1,9 +1,8 @@
 # all the code pointing to the roboflow inference API with our object detection model
 from roboflow import Roboflow
-#different comment
 rf = Roboflow(api_key="API_KEY") # need to grab key
 project = rf.workspace().project("MODEL_ENDPOINT") # need to grab endpoint
 model = project.version(VERSION).model # need to grab version
-
+#im adding stuff here
 # infer on a local image
 print(model.predict("your_image.jpg", confidence=40, overlap=30).json())
