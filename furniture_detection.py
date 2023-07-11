81befe2aa4b22b059463511674f165c4d0bbaa42
Pixilmon <138802402+Pixilmon@users.noreply.github.com>
2023-07-11 14:27:33 -0400

    Update furniture_detection.py

--- furniture_detection.py
+++ furniture_detection.py
@@ -1,7 +1,7 @@
 # all the code pointing to the roboflow inference API with our object detection model
 from roboflow import Roboflow
 
-rf = Roboflow(api_key="API_KEY") # need to grab key
+rf = Roboflow(api_key="wJ1fFMldaUhe4Ni1w2Gx") # need to grab key
 project = rf.workspace().project("MODEL_ENDPOINT") # need to grab endpoint
 model = project.version(VERSION).model # need to grab version
 
