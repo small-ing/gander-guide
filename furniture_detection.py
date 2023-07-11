f6ff467c7b8ee186d0d3544943d39fba795f2498
Travis Smalling <main@smalling.dev>
2023-07-11 13:20:29 -0500

    added roboflow boilerplate
    copy pasted stuff from their documentation

--- furniture_detection.py
+++ furniture_detection.py
@@ -1,1 +1,9 @@
 # all the code pointing to the roboflow inference API with our object detection model
+from roboflow import Roboflow
+
+rf = Roboflow(api_key="API_KEY") # need to grab key
+project = rf.workspace().project("MODEL_ENDPOINT") # need to grab endpoint
+model = project.version(VERSION).model # need to grab version
+
+# infer on a local image
+print(model.predict("your_image.jpg", confidence=40, overlap=30).json())
